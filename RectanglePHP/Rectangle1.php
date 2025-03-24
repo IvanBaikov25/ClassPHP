17bf6afbe4b7d204152549769de9a6370dad794f
IvanBaikov25 <bajkovivan07@gmail.com>
2025-03-24 16:51:24 +0300

    Rectangle1.php

--- RectanglePHP/Rectangle1.php
+++ RectanglePHP/Rectangle1.php
@@ -1,6 +1,7 @@
 <?php
 
-require 'Rectangle.php';
-$myRectangle = new Rectangle(5.0, 3.0);
-echo "Площадь прямоугольника: " . $myRectangle->getArea() . " кв. единиц" . PHP_EOL;
-echo "Периметр прямоугольника: " . $myRectangle->getPerimeter() . " единиц" . PHP_EOL;
+require_once 'Rectangle.php';
+
+$Rectangle = new Rectangle(16.00, 5.00);
+echo "Площадь: " . $Rectangle->getArea() . "\n";
+echo "Периметр: " . $Rectangle->getPerimeter() . "\n";
