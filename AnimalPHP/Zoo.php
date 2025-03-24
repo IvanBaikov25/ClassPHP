c41545dc2fa9cffd985b5c2774643ce6647b194f
IvanBaikov25 <bajkovivan07@gmail.com>
2025-03-24 16:53:40 +0300

    Update Zoo.php

--- AnimalPHP/Zoo.php
+++ AnimalPHP/Zoo.php
@@ -1,23 +1,27 @@
 <?php
 
-require 'Animal.php';
+require_once 'Animal.php';
 
-class Zoo {
-    private array $animals = [];
+class Zoo
+{
+ private array $animals = [];
 
-    public function addAnimal(Animal $animal): void {
-        $this->animals[] = $animal;
-    }
+ public function addAnimal(Animal $animal): void
+ {
+  $this->animals[] = $animal;
+ }
 
-    public function listAnimals(): void {
-        foreach ($this->animals as $animal) {
-            echo $animal->getInfo() . PHP_EOL;
-        }
-    }
+ public function listAnimals(): void
+ {
+  foreach ($this->animals as $animal) {
+   echo $animal->getInfo() . "\n";
+  }
+ }
 
-    public function animalSounds(): void {
-        foreach ($this->animals as $animal) {
-            $animal->makeSound();
-        }
-    }
+ public function animalSounds(): void
+ {
+  foreach ($this->animals as $animal) {
+   $animal->makeSound();
+  }
+ }
 }
