00ae0ac23e7c9e0648f051b1a96113dd6ace259c
IvanBaikov25 <bajkovivan07@gmail.com>
2025-03-24 16:49:30 +0300

    Car.php

--- CarPHP/Car.php
+++ CarPHP/Car.php
@@ -1,29 +1,32 @@
 <?php
 
-class Car {
-    private string $brand;
-    private string $model;
-    private int $year;
-    private int $mileage;
+class Car
+{
+ private string $brand;
+ private string $model;
+ private int $year;
+ private int $mileage;
 
-    public function __construct(string $brand, string $model, int $year, int $mileage) {
-        $this->brand = $brand;
-        $this->model = $model;
-        $this->year = $year;
-        $this->mileage = $mileage;
-    }
+ public function __construct(string $brand, string $model, int $year, int $mileage)
+ {
+  $this->brand = $brand;
+  $this->model = $model;
+  $this->year = $year;
+  $this->mileage = $mileage;
+ }
 
-    public function getInfo(): string {
-        return "Машина: {$this->brand}, модель: {$this->model}, год: {$this->year}, пробег: {$this->mileage} км";
-    }
+ public function getInfo(): string
+ {
+  return "Машина: {$this->brand}, модель: {$this->model}, год: {$this->year}, пробег: {$this->mileage} км";
+ }
 
-    public function drive(int $distance): void {
-        if ($distance > 0) {
-            $this->mileage += $distance;
-        }
-    }
+ public function drive(int $kilometers): void
+ {
+  $this->mileage += $kilometers;
+ }
 
-    public function getMileage(): int {
-        return $this->mileage;
-    }
+ public function getMileage(): int
+ {
+  return 35000;
+ }
 }
