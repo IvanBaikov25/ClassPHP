e3d8bfb7f80000e5769f3fdd9a06603081ad1821
IvanBaikov25 <bajkovivan07@gmail.com>
2025-03-24 16:52:37 +0300

    Cat.php

--- AnimalPHP/Cat.php
+++ AnimalPHP/Cat.php
@@ -1,20 +1,24 @@
 <?php
 
-require 'Animal.php';
+require_once 'Animal.php';
 
-class Cat extends Animal {
-    private string $color;
+class Cat extends Animal
+{
+ private string $color;
 
-    public function __construct(string $name, int $age, string $species, string $color) {
-        parent::__construct($name, $age, $species);
-        $this->color = $color;
-    }
+ public function __construct(string $name, int $age, string $color)
+ {
+  parent::__construct(name: $name, age: $age, species: "Кошка");
+  $this->color = $color;
+ }
 
-    public function makeSound(): void {
-        echo "Мяу!" . PHP_EOL;
-    }
+ public function makeSound(): void
+ {
+  echo "Мяу!\n";
+ }
 
-    public function getInfo(): string {
-        return parent::getInfo() . ", Цвет: {$this->color}";
-    }
+ public function getInfo(): string
+ {
+  return parent::getInfo() . ", Цвет: {$this->color}";
+ }
 }
